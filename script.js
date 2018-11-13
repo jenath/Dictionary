516b13f2bf9a6eecb94229160364bcfc524081b6
Your Name <jenathkanagesan@gmail.com>
2018-11-13 12:25:39 +0000

    #6 Card display created & save button added

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-  window.onload = () => {
+window.onload = () => {
   const searchBtn = document.querySelector("#searchBtn");
   const query = document.querySelector("#query");
   const queryfield = document.querySelector(".queryfield");
 
-  // request to Oxford dictionary API
-  // https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=your-api-key
+  let saveBtn = document.createElement('button');
+  saveBtn.textContent = 'save';
+
 
   queryfield.addEventListener('click', _ => {
     query.focus();
   });
+
+  // request to Oxford dictionary API
+  // https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=your-api-key
 
   searchBtn.addEventListener('click', _ => {
     // if a description of a word is displayed remove it before adding the next word searched
@@ -16,17 +20,31 @@
     if (card) card.parentNode.removeChild(card);
 
     // user search word and API call using the users imput
-    const word = query.value;
+    let word = query.value;
     let url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=1b93ab08-66da-40b0-be26-fe48d7400286`;
 
     // API call to retrieve data & display it on the page
     fetch(url).then(resp => {
       return resp.json();
     }).then(data => {
+
+      // let card = document.createElement('article');
+      // document.body.appendChild(card);
+      // card.setAttribute("id", "card");
+      // card.innerHTML = `<h1>${word}</h1><p>${data[0].shortdef[0]}</p>`;
+
       let card = document.createElement('section');
+      let h1 = document.createElement('h1');
+      let para = document.createElement('p');
       let desc = document.createTextNode(data[0].shortdef[0]);
+      word = word.charAt(0).toUpperCase() + word.slice(1);
+      word = document.createTextNode(word);
+      h1.appendChild(word);
+      h1.appendChild(saveBtn);
+      para.appendChild(desc);
+      card.appendChild(h1);
+      card.appendChild(para);
       card.setAttribute("id", "card");
-      card.appendChild(desc);
       document.body.appendChild(card);
     }).catch(err => {
       let card = document.createElement('section');
